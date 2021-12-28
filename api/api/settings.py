816bdf8bdcf99b73b78de91305ea7b2d3e8b1322
ram.sinha <t-ramprasad.sinha@paypay-corp.co.jp>
2021-12-28 19:43:39 +0530

    dbLIVE

--- api/api/settings.py
+++ api/api/settings.py
@@ -77,13 +77,23 @@ WSGI_APPLICATION = 'api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'edusloggDB',
+        'USER':'eduslogg',
+        'PASSWORD':'Arianna22',
+        'HOST':'database-1.cxmexhvprtnf.us-east-2.rds.amazonaws.com',
+        'PORT':'5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
